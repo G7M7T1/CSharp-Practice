c4a0262ed685243fd0504a6f21669d76d04d9005
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2022-10-18 16:55:12 -0400

    Update Implicityly Typed Variables 1.cs

--- Extension/Implicityly Typed Variables 1.cs
+++ Extension/Implicityly Typed Variables 1.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace ImplictlyTypedVariable
 {
     internal class Program
@@ -8,6 +10,13 @@ namespace ImplictlyTypedVariable
             p1 = new namespace1.namespace2.namespace3.Person() { PersonName = "Yun" };
 
             var p2 = new namespace1.namespace2.namespace3.Person() { PersonName = "Lis" };
+
+            var p3 = "Lis".ToUpper();
+            Console.WriteLine(p3);
+            Console.WriteLine(p2.PersonName);
+            Console.WriteLine(p1.PersonName);
+
+            Console.ReadKey();
         }
     }
 }
