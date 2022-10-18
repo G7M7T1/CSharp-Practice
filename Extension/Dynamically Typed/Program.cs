641414224fce5c3bd99734543bba2b8e46781bde
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2022-10-18 17:08:47 -0400

    Create Program.cs

---
+++ Extension/Dynamically Typed/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DynamicallyTypedVariablesExample
+{
+    class Student
+    {
+        public string StudentName { get; set; }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            //dynamically typed variable
+            dynamic x;
+
+            x = 100;
+
+            x = "Hello";
+
+            x = new Student() { StudentName = "Harsha" };
+            Student y = new Student() { StudentName = "Lis" };
+
+            Console.WriteLine(x.StudentName);
+            Console.WriteLine(y.StudentName);
+            
+            Console.ReadKey();
+        }
+    }
+}
